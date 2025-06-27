11ce2bdf7a4a301beeee7ec582df6cc852127ef5
Steve Wang <steve.john.wang@gmail.com>
2025-06-26 20:26:04 -0400

    Fix sticky bar.

--- js/common.js
+++ js/common.js
@@ -233,7 +233,7 @@ $.getScript("https://cdn.rawgit.com/google/code-prettify/master/loader/run_prett
 });
 
 // Collapsible navbar menu, using https://github.com/jordnkr/collapsible
-$.getScript("/js/jquery.collapsible.js", function(){
+$.getScript("/abseil.github.io/js/jquery.collapsible.js", function(){
   highlightActive();
   $('.submenu').collapsible();
 });
